d08339d4267059b30c05035dde9fba669a2e4d78
Dario Narvaez <narvaezdario91@gmail.com>
2026-07-26 13:19:32 -0500

    fix: configurar site y base en astro.config para despliegue correcto de assets en github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://narvaezdario91.github.io',
+  base: '/narvaezdario_web_page',
   integrations: [react()],
 });
